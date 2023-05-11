93488e4622df60b1111116569d0a3d4c3ca61403
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-05-11 08:56:56 +0200

    idk

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Vampire Studios\' Blog',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
